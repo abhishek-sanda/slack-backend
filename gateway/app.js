e8b1d757cb3608e2a0c8d05874898c6e3dc7ea6b
abhishek-sanda <abhisheksanda@gmail.com>
2026-01-17 14:12:24 +0530

    Update app.js

--- gateway/app.js
+++ gateway/app.js
@@ -5,7 +5,9 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 require("dotenv").config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin:"https://slack-frontend-five.vercel.app"
+}));
 app.use(express.json());
 app.use(morgan("dev"));
 
