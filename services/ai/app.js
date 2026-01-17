4231cf54d49e29a2e836017638c58192500b33b0
abhishek-sanda <abhisheksanda@gmail.com>
2026-01-17 14:13:35 +0530

    Update app.js

--- services/ai/app.js
+++ services/ai/app.js
@@ -5,7 +5,9 @@ const aiRoutes = require("./routes/aiRoutes");
 require('dotenv').config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin:"https://slack-frontend-five.vercel.app"
+}));
 app.use(express.json({ limit: '100mb' }));
 app.use(express.urlencoded({ extended: true, limit: '100mb' }));
 
